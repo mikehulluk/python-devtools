56fdf270c2a3cab2e7807386f56a2771e56f4c54
Michael Hull <mikehulluk@gmail.com>
2015-08-29 15:04:03 +0100

    Refactored patching code

--- hdt/src/ActionApply.hs
+++ hdt/src/ActionApply.hs
@@ -8,6 +8,7 @@ import HdtTypes
 import CmdLineOpts
 import HdtProject
 import HdtFilePatchStack
+import ExtTools
 
 import System.IO.Temp
 import System.IO
@@ -17,26 +18,64 @@ import System.Exit
 import System.Directory
 
 import qualified Data.ByteString.Char8 as B
+import Database.SQLite.Simple
+import Data.Maybe
 
-import ExtTools
+
+
+
+
 
 execApply :: MyOptions -> IO ()
 execApply opts@ModeApply{..} = do
-
     projects <- getAllProjectConfigs
     let activeProjects = filter isActive projects
     let proj = head activeProjects
-    files <- srcFiles proj
-    mapM_ applyFile files
-    --return()
+    --files <- srcFiles proj
+    --mapM_ applyFile files
 
 
+    merges <- getProjectFileMergeInfos proj
+    applyProjectFileMergeInfos merges
+
+
+data FileMergeInfo = FileMergeInfo {
+      file    :: File
+    , newBlob :: String
+} deriving (Show)
 
 
 uiMergeFile :: String -> String -> IO ()
 uiMergeFile fname newBlob  = do
     exitCode <- withTempFile "/home/michael/.hdt/" "tmp.mergefile" (runMergeTool fname newBlob)
     putStrLn $ " --- Finished with exit code: " ++ show exitCode
+
+
+getFileMergeInfo :: Connection -> File -> IO ( Maybe FileMergeInfo)
+getFileMergeInfo dbConn file = do
+    patches <- getFilePatchs dbConn file
+    case length patches of 
+        0 -> return Nothing
+        _ -> do
+            newBlob <- mergePatches file patches
+            return $ Just FileMergeInfo{file=file,newBlob=newBlob}
+
+
+getProjectFileMergeInfos :: Project -> IO [ Maybe FileMergeInfo] 
+getProjectFileMergeInfos proj = do
+    dbConn <- getProjectDBHandle proj
+    files <- srcFiles proj
+    infos <- mapM (getFileMergeInfo dbConn) files
+    return infos
+
+applyFileMergeInfo :: FileMergeInfo -> IO()
+applyFileMergeInfo mergeInfo = uiMergeFile (filename $ file mergeInfo) (newBlob mergeInfo)
+
+
+applyProjectFileMergeInfos :: [Maybe FileMergeInfo] -> IO()
+applyProjectFileMergeInfos  infos = do
+    mapM_ applyFileMergeInfo (catMaybes infos)
+
 
 
 
@@ -57,8 +96,6 @@ applyFile file = do
 
             -- 2. Write it to a tempfile, and run the mergetool
             uiMergeFile fname newBlob
-            --exitCode <- withTempFile "/home/michael/.hdt/" "tmp.mergefile" (runMergeTool (filename file) finalBlob)
-            --putStrLn $ " --- Finished with exit code: " ++ show exitCode
 
             -- 3. If it completed ok, then remove the diffs from the database.
             dropOutstandingPatchs file
@@ -78,14 +115,6 @@ applyFile file = do
 
 
 
-mergePatchList :: B.ByteString -> [B.ByteString] -> IO (Maybe B.ByteString)
-mergePatchList originalBlob []  = return $ Just originalBlob
-mergePatchList originalBlob [p] = return =<< (runExtPatch originalBlob p)
-mergePatchList originalBlob (p:ps) = do
-    pch <- runExtPatch originalBlob p
-    case pch of
-        Nothing  -> return $ Nothing
-        Just res -> return =<< (mergePatchList res ps)
 
 
 
@@ -121,5 +150,13 @@ mergePatches file patches = do
 
 
 
+mergePatchList :: B.ByteString -> [B.ByteString] -> IO (Maybe B.ByteString)
+mergePatchList originalBlob []  = return $ Just originalBlob
+mergePatchList originalBlob [p] = return =<< (runExtPatch originalBlob p)
+mergePatchList originalBlob (p:ps) = do
+    pch <- runExtPatch originalBlob p
+    case pch of
+        Nothing  -> return $ Nothing
+        Just res -> return =<< (mergePatchList res ps)
 
 
