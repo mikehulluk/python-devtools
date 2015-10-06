40dad251b76356e194e14546e3a07b26c2b91cfc
Michael Hull <mikehulluk@gmail.com>
2015-10-06 22:23:11 +0100

    ...

--- hdt/bin/hdt_wrapper.sh
+++ hdt/bin/hdt_wrapper.sh
@@ -5,7 +5,7 @@
 # the arguments straight through, but we want to enable a 'go' command, 
 # which is going to 'cd' into the projects root directory.
 # Since we can't change the directory of a parent process, we need to source this
-# within the active shell:
+# within the active shell.
 
 case "$1" in 
     go)
