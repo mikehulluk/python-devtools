8c16617b25391c848bd20b0e55b8ad20c28747c2
Michael Hull <mikehulluk@gmail.com>
2015-10-06 22:11:26 +0100

    Added wrapper script

--- hdt/bin/hdt_wrapper.sh
+++ hdt/bin/hdt_wrapper.sh
@@ -1,11 +1,22 @@
 
-echo $1
+# hdt_wrapper.sh
+# ---------------
+# A simple wrapper script around hdt. In most cases, it just passes
+# the arguments straight through, but we want to enable a 'go' command, 
+# which is going to 'cd' into the projects root directory.
+# Since we can't change the directory of a parent process, we need to source this
+# within the active shell:
 
 case "$1" in 
     go)
         echo "GOTO'ing"
+        tgtdir=$(hdt getrootdir)
+        echo "Switching into : $tgtdir"
+        cd $tgtdir
         ;;
     *)
         echo "Std Cmd'ing"
+        echo Cmd:  hdt "$@"
+        hdt "$@"
         ;;
 esac
